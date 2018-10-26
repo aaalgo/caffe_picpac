3e5d4c9b4e56797f9ad53182f0b2ffff446bdabc
Wei Dong <wdong@wdong.org>
2018-10-25 23:37:41 -0400

    added prediction code

---
+++ predict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import numpy as np
+import cv2
+import caffe
+
+WIDTH = 640
+HEIGHT = 400
+
+net = caffe.Net('deploy.prototxt', 'snapshots/x_iter_2000.caffemodel', caffe.TEST)
+
+image = cv2.imread('test.jpg', cv2.IMREAD_COLOR)
+image = cv2.resize(image, (WIDTH,HEIGHT))
+# caffe dimension order is NCHW
+batch = np.transpose(image[np.newaxis, :, :, :], [0, 3, 1, 2])
+net.blobs['data'].data[...] = batch
+out = net.forward()
+prob = out['prob'][0, 1, :, :]  # channel 1 is probability
+cv2.imwrite('prob.jpg', prob * 256)
+
+
